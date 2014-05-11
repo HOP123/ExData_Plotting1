d91507705750eb3fa0c11427ded425cb92fb527b
HOP123 <hisayo-powell@comcast.net>
2014-05-10 18:43:09 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,25 @@
+
+#read file and save to myData
+lines <- grep('^[1-2]/2/2007', readLines('household_power_consumption.txt'))
+myData <- read.table(file="household_power_consumption.txt", sep=";", skip=66637, nrow=2880)
+
+#save myData to usethis file (I can play with use this file)
+usethis <- myData
+
+#combine Date and Time and convert it to POSIXtl object
+dateTime <- as.character(paste(usethis[,1], usethis[,2], sep=" "))
+dateTime <- strptime(dateTime, "%d/%m/%Y %H:%M:%S")
+
+#combine dateTime and usethis and order it by dateTime col
+usethis1 <- cbind(dateTime, usethis)
+usethis2 <- usethis1[order(usethis1$dateTime),]
+colnames(usethis2) <- c("dateTime", "Date", "Time", "GAP","GRP", "Vol", "GI", "SM1","SM2","SM3")
+
+#plot1
+png(file="plot1.png", width=480, height=480)
+par(mfrow=c(1,1), mar=c(4,4,2,2))
+hist(usethis2$GAP, breaks=12, col="red", main="Global Active Power",ylab="Frequency", xlab="Global Active Power (Kilowatts)")
+dev.off()
+
+
+
